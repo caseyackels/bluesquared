2908fd3547941d2a181b4058629f6a1c99f6d652
caseyackels <casey.ackels@gmail.com>
2015-03-24 07:08:23 -0700

    Remove Builds dir

--- Builds/EfficiencyAssist.vfs/lib/app-efficiencyassist/Libraries/debug/pkgIndex.tcl
+++
@@ -1,1 +0,0 @@
-package ifneeded debug 1.0 [list source [file join $dir debug.tcl]]
--- Builds/EfficiencyAssist.vfs/lib/app-efficiencyassist/Modules/Update/pkgIndex.tcl
+++
@@ -1,2 +0,0 @@
-package ifneeded vUpdate 1.0 "[list source [file join $dir detect_updates_code.tcl]]
-                                [list source [file join $dir detect_updates_gui.tcl]]"
