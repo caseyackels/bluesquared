1f9723c72c5ac80036f68895e9946316b915ab99
caseyackels <casey.ackels@gmail.com>
2015-07-27 07:44:44 -0700

    Company - Delete accounts is now working

--- EfficiencyAssist.vfs/lib/app-efficiencyassist/Modules/Core/setup_company_code.tcl
+++ EfficiencyAssist.vfs/lib/app-efficiencyassist/Modules/Core/setup_company_code.tcl
@@ -60,3 +60,364 @@ proc eAssistSetup::populateCompanyArray {} {
 
 
 } ;# eAssistSetup::populateCompanyArray
+
+
+proc eAssistSetup::editCompany {f editMode btn} {
+    #****f* editCompany/eAssistSetup
+    # CREATION DATE
+    #   07/22/2015 (Wednesday Jul 22)
+    #
+    # AUTHOR
+    #	Casey Ackels
+    #
+    # COPYRIGHT
+    #	(c) 2015 Casey Ackels
+    #   
+    #
+    # USAGE
+    #   eAssistSetup::editCompany f 
+    #
+    # FUNCTION
+    #	Makes the Company fields editable/readonly submits changes to the DB
+    #   edit mode: changes state to normal, changes button text to 'Update'
+    #   readonly mode: changes state to readonly; submits data to database; changes button text to 'Change'
+    #   
+    #   
+    # CHILDREN
+    #	N/A
+    #   
+    # PARENTS
+    #   
+    #   
+    # EXAMPLE
+    #   eAssistSetup::editCompany $f1 normal|readonly
+    #
+    # NOTES
+    #   
+    #  
+    # SEE ALSO
+    #   
+    #   
+    #***
+    global log masterAddr
+
+    ${log}::debug State $editMode
+    
+    foreach child [winfo child $f] {
+        $child configure -state $editMode
+    }
+    
+    if {$editMode eq "readonly"} {
+        # We are making a change (adding/updating company info)
+        # Update DB
+        ${log}::debug Update DB with masterAddr() array
+        # Reconfigure Button
+        #${log}::debug $btn configure -command editCompany $f normal $btn
+        $btn configure -text [mc "Change"] -command [list eAssistSetup::editCompany $f normal $btn]
+        
+        ${log}::debug Check DB for Company Address
+        set companyExists [db eval "SELECT MasterAddr_ID, MasterAddr_Plant FROM MasterAddresses"]
+        if {$companyExists eq ""} {
+            ${log}::debug Company doesn't exist, adding to the database...
+            set masterAddr(Plant) 1
+            
+            if {[info exists colList]} {unset colList}
+            if {[info exists varList]} {unset varList}
+            foreach dbCol [lsort [array names masterAddr]] {
+                if {$dbCol eq "ID"} {continue}; #skip, this is only used when we pull data out, not putting it in.
+                lappend colList MasterAddr_$dbCol
+            }
+            
+            set masterAddrArrayNames [lsort [array names masterAddr]]
+            foreach var $masterAddrArrayNames {
+                if {$var eq "ID"} {continue}; #skip, this is only used when we pull data out, not putting it in.
+                lappend varList '$masterAddr($var)'
+            }
+            #${log}::debug INSERT INTO MasterAddresses ([join $colList ,]) VALUES ([join $varList ,])
+            db eval "INSERT INTO MasterAddresses ([join $colList ,]) VALUES ([join $varList ,])"
+            
+            #parray masterAddr
+        } else {
+            ${log}::debug Company exists in the database, updating address...
+            if {[info exists colList]} {unset colList}
+            if {[info exists dbValues]} {unset dbValues}
+                
+            foreach dbCol [lsort [array names masterAddr]] {
+                if {$dbCol eq "ID"} {continue}; #skip, this is only used when we pull data out, not putting it in.
+                lappend colList MasterAddr_$dbCol
+            }
+            
+            foreach val $colList {
+                set arrayName [lrange [split $val _] 1 1]
+                lappend dbValues $val='$masterAddr($arrayName)'
+            }
+            
+            #${log}::debug db eval "UPDATE MasterAddresses SET [join $dbValues ,] WHERE MasterAddr_ID=$masterAddr(ID)"
+            db eval "UPDATE MasterAddresses SET [join $dbValues ,] WHERE MasterAddr_ID=$masterAddr(ID)"
+        }
+                
+        
+    } else {
+        #${log}::debug $btn configure -command editCompany $f readonly $btn
+        
+        # setting window to readonly - just need to update the button command
+        $btn configure -text [mc "Save"] -command [list eAssistSetup::editCompany $f readonly $btn]
+    }
+ 
+} ;# eAssistSetup::editCompany
+
+
+proc eAssistSetup::editCarrier {widTbl carrierWid acctWid} {
+    #****f* editCarrier/eAssistSetup
+    # CREATION DATE
+    #   07/24/2015 (Friday Jul 24)
+    #
+    # AUTHOR
+    #	Casey Ackels
+    #
+    # COPYRIGHT
+    #	(c) 2015 Casey Ackels
+    #   
+    #
+    # USAGE
+    #   eAssistSetup::editCarrier widTbl carrierWid acctWid 
+    #
+    # FUNCTION
+    #	Takes the values from the Carrier and Account entry field, and insert them into the DB/Table.
+    #   
+    #   
+    # CHILDREN
+    #	N/A
+    #   
+    # PARENTS
+    #   
+    #   
+    # EXAMPLE
+    #   eAssistSetup::editCarrier $widTbl $carrierWid $accountWid
+    #
+    # NOTES
+    #   
+    #  
+    # SEE ALSO
+    #   
+    #   
+    #***
+    global log masterAddr tmp
+
+    # Do we have a masterID?
+    if {$masterAddr(ID) == "" } {${log}::debug Master Addr is not set...; return}
+    
+    # Get carrier Name
+    set carrierName [$carrierWid get]
+    if {$carrierName == "" } {${log}::debug Please choose a carrier...; return}
+    
+    # Get carrier ID
+    set carrierID [db eval "SELECT Carrier_ID FROM Carriers WHERE Name = '$carrierName'"]
+    
+    # Get carrier Account
+    set carrierAcct [$acctWid get]
+    if {$carrierAcct == "" } {${log}::debug Please enter an account...; return}
+    
+    # Clear data from widgets
+    $carrierWid delete 0 end
+    $acctWid delete 0 end
+    
+    
+    ## Updating or Inserting?
+    ##
+    if {![info exists tmp(db,id)]} {set tmp(db,id) ""}
+    if {$tmp(db,id) == ""} {
+        # Insert data into DB
+        #${log}::debug db eval "INSERT INTO CarrierAccts (CarrierAccts_Acct, MasterAddrID, CarrierID) VALUES ('$carrierAcct', '$masterAddr(ID)', '$carrierID')"
+        db eval "INSERT INTO CarrierAccts (CarrierAccts_Acct, MasterAddrID, CarrierID) VALUES ('$carrierAcct', '$masterAddr(ID)', '$carrierID')"
+    
+    } else {
+        # Updating data
+        db eval "UPDATE CarrierAccts SET CarrierAccts_Acct='$carrierAcct', CarrierID=$carrierID
+                    WHERE CarrierAccts_ID=$tmp(db,id)"
+                    
+        set tmp(db,id) "";# remove this since we just used it and updated info in the db.
+    }
+
+    
+    # Populate tablelist widget
+    #${log}::debug eAssistSetup::populateCarrierTbl $widTbl
+    eAssistSetup::populateCarrierTbl $widTbl
+    
+} ;# eAssistSetup::editCarrier
+
+
+proc eAssistSetup::populateCarrierTbl {widTbl} {
+    #****f* populateCarrierTbl/eAssistSetup
+    # CREATION DATE
+    #   07/24/2015 (Friday Jul 24)
+    #
+    # AUTHOR
+    #	Casey Ackels
+    #
+    # COPYRIGHT
+    #	(c) 2015 Casey Ackels
+    #   
+    #
+    # USAGE
+    #   eAssistSetup::populateCarrierTbl widTbl 
+    #
+    # FUNCTION
+    #	Populates the table widget for the carrier accounts
+    #   
+    #   
+    # CHILDREN
+    #	N/A
+    #   
+    # PARENTS
+    #   
+    #   
+    # EXAMPLE
+    #   eAssistSetup::populateCarrierTbl 
+    #
+    # NOTES
+    #   
+    #  
+    # SEE ALSO
+    #   
+    #   
+    #***
+    global log masterAddr
+    
+    # remove data from the widget first; if we don't data will be duplicated
+    $widTbl delete 0 end
+
+    # Retrieve data from DB
+    db eval "SELECT CarrierID, CarrierAccts_Acct, Active, Carriers.Name as Name from CarrierAccts
+                INNER JOIN Carriers ON
+                Carrier_ID = CarrierID
+                WHERE Active = 1
+                AND MasterAddrID = $masterAddr(ID)" {
+                    $widTbl insert end [list "" $Name $CarrierAccts_Acct]
+                }
+
+    
+} ;# eAssistSetup::populateCarrierTbl .container.setup.fc0.fc2.tbl
+
+
+proc eAssistSetup::modifyCarrier {tbl carrier account} {
+    #****f* modifyCarrier/eAssistSetup
+    # CREATION DATE
+    #   07/24/2015 (Friday Jul 24)
+    #
+    # AUTHOR
+    #	Casey Ackels
+    #
+    # COPYRIGHT
+    #	(c) 2015 Casey Ackels
+    #   
+    #
+    # USAGE
+    #   eAssistSetup::modifyCarrier tbl carrier account 
+    #
+    # FUNCTION
+    #	Removes any data in the Carrier and Account entry fields, then populates the Carrier and Account field from the data that was double clicked on
+    #   
+    #   
+    # CHILDREN
+    #	N/A
+    #   
+    # PARENTS
+    #   
+    #   
+    # EXAMPLE
+    #   eAssistSetup::modifyCarrier tbl carrier account
+    #
+    # NOTES
+    #   
+    #  
+    # SEE ALSO
+    #   
+    #   
+    #***
+    global log masterAddr tmp
+
+    ${log}::debug [$tbl get [$tbl curselection]]
+    set getData [$tbl get [$tbl curselection]]
+    $carrier delete 0 end
+    $carrier insert end [lrange $getData 1 1]
+    
+    $account delete 0 end
+    $account insert end [lrange $getData 2 2]
+    
+    set carrierID [db eval "SELECT Carrier_ID FROM Carriers WHERE Name = '[lrange $getData 1 1]'"]
+    set tmp(db,id) [db eval "SELECT CarrierAccts_ID FROM CarrierAccts
+                                WHERE MasterAddrID = $masterAddr(ID)
+                                AND CarrierID = $carrierID
+                                AND CarrierAccts_Acct = '[lrange $getData 2 2]'"]
+
+    
+} ;# eAssistSetup::modifyCarrier
+
+proc eAssistSetup::deleteCarrier {tbl} {
+    #****f* deleteCarrier/eAssistSetup
+    # CREATION DATE
+    #   07/27/2015 (Monday Jul 27)
+    #
+    # AUTHOR
+    #	Casey Ackels
+    #
+    # COPYRIGHT
+    #	(c) 2015 Casey Ackels
+    #   
+    #
+    # USAGE
+    #   eAssistSetup::deleteCarrier tbl 
+    #
+    # FUNCTION
+    #	Removes the selected entry from the database, then calls [eAssistSetup::populateCarrierTbl] to update the widget
+    #   
+    #   
+    # CHILDREN
+    #	N/A
+    #   
+    # PARENTS
+    #   
+    #   
+    # EXAMPLE
+    #   eAssistSetup::deleteCarrier 
+    #
+    # NOTES
+    #   
+    #  
+    # SEE ALSO
+    #   
+    #   
+    #***
+    global log
+
+    #${log}::debug selection: [$tbl get [$tbl curselection]]
+    
+    set carrierName [lindex [$tbl get [$tbl curselection]] 1]
+    set carrierAcct [lindex [$tbl get [$tbl curselection]] 2]
+    
+    #${log}::debug carrierName: $carrierName
+    #${log}::debug carrierAcct: $carrierAcct
+    
+    set carrierID [db eval "SELECT Carrier_ID FROM Carriers WHERE Name = '$carrierName'"]
+    
+    #set dbCheck ""
+    
+    # Check the db - Unneeded?? (7/2015)
+    #set dbCheck [db eval "SELECT CarrierAccts_Acct, CarrierID FROM CarrierAccts
+    #                        WHERE CarrierAccts_Acct = '$carrierAcct'
+    #                        AND CarrierID = $carrierID"]
+    
+    #${log}::debug dbCheck: $dbCheck
+    
+    
+    db eval "DELETE FROM CarrierAccts
+                    WHERE CarrierAccts_Acct = '$carrierAcct'
+                            AND CarrierID = $carrierID"
+    
+    
+    ${log}::notice [mc "Company Setup, Deleted Account: $carrierAcct"]
+
+    eAssistSetup::populateCarrierTbl $tbl
+    
+} ;# eAssistSetup::deleteCarrier
--- EfficiencyAssist.vfs/lib/app-efficiencyassist/Modules/Core/setup_company_gui.tcl
+++ EfficiencyAssist.vfs/lib/app-efficiencyassist/Modules/Core/setup_company_gui.tcl
@@ -61,51 +61,86 @@ proc eAssistSetup::company_GUI {{widType embed}} {
     ##
     ## Frame setup
     ##
-    #-------- Address fields frame
+    #-------- Container Address fields frame
     set fa0 [ttk::labelframe $win.fa0 -text [mc "Company Address"]]
     pack $fa0 -expand yes -fill both -pady 5p -padx 5p
     
+    #-------- Address fields frame
+    set fa1 [ttk::frame $fa0.fa1]
+    pack $fa1 -expand yes -fill both -padx 5p
     
-    #-------- Carrier accounts frame
+    #-------- Change/Update button
+    set fa2 [ttk::frame $fa0.fa2]
+    pack $fa2 -anchor se -pady 2p -padx 5p
+    
+    
+    #-------- Carrier accounts main frame
     set fc0 [ttk::labelframe $win.fc0 -text [mc "Carrier Accounts"]]
     pack $fc0 -expand yes -fill both -pady 5p -padx 5p
     
+    set fc1 [ttk::frame $fc0.fc1] ;# frame for the add/delete buttons
+    pack $fc1 -expand yes -fill both
+    
+    set fc2 [ttk::frame $fc0.fc2] ;# Frame for the tablelist
+    pack $fc2 -expand yes -fill both
+    
     #-------- Button frame
     # Initially see the "change" button. Once clicked, it changes to "OK". All fields start out 'disabled'
-    set fb0 [ttk::frame $win.fb0]
-    pack $fb0 -anchor sw -pady 5p -padx 5p
+    #set fb0 [ttk::frame $win.fb0]
+    #pack $fb0 -anchor sw -pady 5p -padx 5p
     
     ## Address fields
     ## 
-    grid [ttk::label $fa0.txt_company -text [mc "Company"]] -column 0 -row 0 -padx 2p -pady 2p -sticky nse
-    grid [ttk::entry $fa0.entry_company -textvariable masterAddr(Company) -width 35] -column 1 -columnspan 3 -row 0 -padx 2p -pady 2p -sticky news
+    grid [ttk::label $fa1.txt_company -text [mc "Company"]] -column 0 -row 0 -padx 2p -pady 2p -sticky nse
+    grid [ttk::entry $fa1.entry_company -textvariable masterAddr(Company) -width 35] -column 1 -columnspan 3 -row 0 -padx 2p -pady 2p -sticky news
     #grid [ttk::checkbutton $fa0.ckbtn_plant -text [mc "Plant"] -variable masterAddr(Plant)] -column 4 -row 0 -padx 2p -pady 2p -sticky nsw
     
-    grid [ttk::label $fa0.txt_attention -text [mc "Attention/Phone"]] -column 0 -row 1 -padx 2p -pady 2p -sticky nse
-    grid [ttk::entry $fa0.entry_attention -textvariable masterAddr(Attn)] -column 1 -columnspan 2  -row 1 -padx 2p -pady 2p -sticky news
-    grid [ttk::entry $fa0.entry_phone -textvariable masterAddr(Phone)] -column 3 -row 1 -padx 2p -pady 2p -sticky news
+    grid [ttk::label $fa1.txt_attention -text [mc "Attention/Phone"]] -column 0 -row 1 -padx 2p -pady 2p -sticky nse
+    grid [ttk::entry $fa1.entry_attention -textvariable masterAddr(Attn)] -column 1 -columnspan 2  -row 1 -padx 2p -pady 2p -sticky news
+    grid [ttk::entry $fa1.entry_phone -textvariable masterAddr(Phone)] -column 3 -row 1 -padx 2p -pady 2p -sticky news
     
-    grid [ttk::label $fa0.txt_addr -text [mc "Address"]] -column 0 -row 2 -padx 2p -pady 2p -sticky nse
-    grid [ttk::entry $fa0.entry_addr1 -textvariable masterAddr(Addr1)] -column 1 -columnspan 3  -row 2 -padx 2p -pady 2p -sticky news
-    grid [ttk::entry $fa0.entry_addr2 -textvariable masterAddr(Addr2)] -column 1 -columnspan 3  -row 3 -padx 2p -pady 2p -sticky news
-    grid [ttk::entry $fa0.entry_addr3 -textvariable masterAddr(Addr3)] -column 1 -columnspan 3  -row 4 -padx 2p -pady 2p -sticky news
+    grid [ttk::label $fa1.txt_addr -text [mc "Address"]] -column 0 -row 2 -padx 2p -pady 2p -sticky nse
+    grid [ttk::entry $fa1.entry_addr1 -textvariable masterAddr(Addr1)] -column 1 -columnspan 3  -row 2 -padx 2p -pady 2p -sticky news
+    grid [ttk::entry $fa1.entry_addr2 -textvariable masterAddr(Addr2)] -column 1 -columnspan 3  -row 3 -padx 2p -pady 2p -sticky news
+    grid [ttk::entry $fa1.entry_addr3 -textvariable masterAddr(Addr3)] -column 1 -columnspan 3  -row 4 -padx 2p -pady 2p -sticky news
     
-    grid [ttk::label $fa0.txt_city -text [mc "City"]] -column 0 -row 5 -padx 2p -pady 2p -sticky nse
-    grid [ttk::entry $fa0.entry_city -textvariable masterAddr(City)] -column 1 -columnspan 3 -row 5 -padx 2p -pady 2p -sticky news
+    grid [ttk::label $fa1.txt_city -text [mc "City"]] -column 0 -row 5 -padx 2p -pady 2p -sticky nse
+    grid [ttk::entry $fa1.entry_city -textvariable masterAddr(City)] -column 1 -columnspan 3 -row 5 -padx 2p -pady 2p -sticky news
     
-    grid [ttk::label $fa0.txt_stateZip -text [mc "State/Zip"]] -column 0 -row 6 -padx 2p -pady 2p -sticky nse
-    grid [ttk::entry $fa0.entry_state -textvariable masterAddr(StateAbbr) -width 4] -column 1 -row 6 -padx 2p -pady 2p -sticky nws
-    grid [ttk::entry $fa0.entry_zip -textvariable masterAddr(Zip)] -column 2 -columnspan 2 -row 6 -padx 2p -pady 2p -sticky ew
+    grid [ttk::label $fa1.txt_stateZip -text [mc "State/Zip"]] -column 0 -row 6 -padx 2p -pady 2p -sticky nse
+    grid [ttk::entry $fa1.entry_state -textvariable masterAddr(StateAbbr) -width 4] -column 1 -row 6 -padx 2p -pady 2p -sticky nws
+    grid [ttk::entry $fa1.entry_zip -textvariable masterAddr(Zip)] -column 2 -columnspan 2 -row 6 -padx 2p -pady 2p -sticky ew
     
-    grid [ttk::label $fa0.txt_country -text [mc "Country Code"]] -column 0 -row 7 -padx 2p -pady 2p -sticky nse
-    grid [ttk::entry $fa0.entry_country -textvariable masterAddr(CtryCode) -width 4] -column 1 -row 7 -padx 2p -pady 2p -sticky nws
+    grid [ttk::label $fa1.txt_country -text [mc "Country Code"]] -column 0 -row 7 -padx 2p -pady 2p -sticky nse
+    grid [ttk::entry $fa1.entry_country -textvariable masterAddr(CtryCode) -width 4] -column 1 -row 7 -padx 2p -pady 2p -sticky nws
      
-    grid [ttk::checkbutton $fa0.ckbtn_active -text [mc "Active"] -variable masterAddr(Active)] -column 1 -columnspan 3 -row 8 -padx 2p -pady 2p -sticky nsw
+    # Make all children read-only
+    foreach child [winfo child $fa1] {
+        ${log}::debug child winfo $child
+        $child configure -state readonly
+    }
+    
+    #grid [ttk::checkbutton $fa0.ckbtn_active -text [mc "Active"] -variable masterAddr(Active)] -column 1 -columnspan 3 -row 8 -padx 2p -pady 2p -sticky nsw
+    grid [ttk::button $fa2.btn -text [mc "Change"] -command [list eAssistSetup::editCompany $fa1 normal $fa2.btn]] -column 3 -sticky se
+    
+    
     
     ## Carrier Accounts
     ##
-    ## Frame $fc0
-    grid [tablelist::tablelist $fc0.tbl -columns {
+    
+    grid [ttk::label $fc1.txtCarrier -text [mc "Carrier"]] -sticky e
+    grid [ttk::label $fc1.txtAccount -text [mc "Account #"]] -sticky e
+    grid [ttk::entry $fc1.carrier \
+                                    -validate all \
+                                    -validatecommand [list AutoComplete::AutoComplete %W %d %v %P [db eval "SELECT Name from Carriers"]]
+                                    ] -column 1 -row 0 -sticky ew
+    grid [ttk::entry $fc1.account] -column 1 -row 1 -sticky ew
+    grid [ttk::button $fc1.btnAdd -text [mc "Add"] -command [list eAssistSetup::editCarrier $fc2.tbl $fc1.carrier $fc1.account]] -column 2 -row 0 -sticky w
+    grid [ttk::button $fc1.btnDel -text [mc "Delete"] -command [list eAssistSetup::deleteCarrier $fc2.tbl]] -column 2 -row 1 -sticky w
+    
+    grid columnconfigure $fc1 1 -weight 1
+    
+    tablelist::tablelist $fc2.tbl -columns {
                                             0 ... center
                                             0 Carrier center
                                             0 Account center
@@ -114,12 +149,23 @@ proc eAssistSetup::company_GUI {{widType embed}} {
                                         -stripebackground yellow \
                                         -showseparators yes \
                                         -fullseparators yes
-                                        ] -column 0 -row 0 -padx 2p -pady 2p -sticky news
+    $fc2.tbl columnconfigure 0 -showlinenumbers yes
+    $fc2.tbl columnconfigure 1 -width 20
+    $fc2.tbl columnconfigure 2 -stretch yes
     
-    $fc0.tbl columnconfigure 0 -showlinenumbers yes
-    $fc0.tbl columnconfigure 2 -stretch yes
+    grid $fc2.tbl -column 0 -columnspan 2 -row 1 -padx 2p -pady 2p -sticky news
+
+    grid columnconfigure $fc2 0 -weight 1
     
-    ## Button
-    ## 
+
+    # Bindings
+    # Setup the bindings
+    set bodyTag [$fc2.tbl bodytag]
+    #bind $bodyTag <Double-1> [list getData [subst $fc2.tbl]]
+    bind $bodyTag <Double-1> "eAssistSetup::modifyCarrier $fc2.tbl $fc1.carrier $fc1.account"
+    #bind $bodyTag <Single-1> "${log}::debug Delete line ..."
+    
+    # Populate tablelist widget
+    eAssistSetup::populateCarrierTbl $fc2.tbl
 
 } ;# eAssistSetup::company_GUI standalone
