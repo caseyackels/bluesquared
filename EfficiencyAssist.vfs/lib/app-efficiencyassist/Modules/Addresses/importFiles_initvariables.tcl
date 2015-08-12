29ad97cc58d9c7b2b4966f7c450c7ab0d18f491f
caseyackels <casey.ackels@gmail.com>
2015-08-12 09:23:38 -0700

    Added a new proc to init vars, and moved another proc
    Moved initVars into initvariables.tcl
    Created setJobArray proc
    
    All variable setting procs are now launched in the initVar proc for
    Batch Maker

--- EfficiencyAssist.vfs/lib/app-efficiencyassist/Modules/Addresses/importFiles_initvariables.tcl
+++ EfficiencyAssist.vfs/lib/app-efficiencyassist/Modules/Addresses/importFiles_initvariables.tcl
@@ -22,6 +22,82 @@
 # - Procedures: Proc names should have two words. The first word lowercase the first character of the first word,
 #   will be uppercase. I.E sourceFiles, sourceFileExample
 
+proc importFiles::initVars {args} {
+    #****f* initVars/importFiles
+    # CREATION DATE
+    #   10/24/2014 (Friday Oct 24)
+    #
+    # AUTHOR
+    #	Casey Ackels
+    #
+    # COPYRIGHT
+    #	(c) 2014 Casey Ackels
+    #   
+    #
+    # SYNOPSIS
+    #   importFiles::initVars args 
+    #
+    # FUNCTION
+    #	Initilize variables
+    #   
+    #   
+    # CHILDREN
+    #	N/A
+    #   
+    # PARENTS
+    #   importFiles::eAssistGUI
+    #   
+    # NOTES
+    #   
+    #   
+    # SEE ALSO
+    #   
+    #   
+    #***
+    global log headerParent headerAddress dist carrierSetup packagingSetup shipOrder
+
+    set headerParent(headerList) [db eval "SELECT dbColName FROM HeadersConfig WHERE dbActive = 1"]
+    
+    set headerParent(headerList,consignee) [db eval "SELECT dbColName FROM HeadersConfig
+                                                        WHERE widUIGroup = 'Consignee'
+                                                        ORDER BY widUIPositionWeight ASC, dbColName ASC"]
+    
+    set headerParent(headerList,shippingorder) [db eval "SELECT dbColName FROM HeadersConfig
+                                                        WHERE widUIGroup <> 'Consignee'
+                                                        ORDER BY widUIPositionWeight ASC, dbColName ASC"]
+    
+    set headerParent(whiteList) [eAssist_db::dbWhereQuery -columnNames dbColName -table HeadersConfig -where widDisplayType='Always']
+    #set headerParent(blackList) [list Status] ;# This is only for the columns that exist in the main table (addresses) that we never want to display
+    set headerParent(ColumnCount) [llength $headerParent(headerList)]
+    set headerParent(ColumnCount,consignee) [db eval "SELECT Count (HeaderConfig_ID) from HeadersConfig
+                                                        WHERE widUIGroup = 'Consignee'"]
+    
+    # Setup header array with subheaders
+    foreach hdr $headerParent(headerList) {
+        # Get the subheaders for the current header
+        set headerAddress($hdr) [db eval "SELECT SubHeaderName FROM HeadersConfig
+                                            LEFT OUTER JOIN SubHeaders
+                                            WHERE HeaderConfigID=HeaderConfig_ID
+                                            AND dbColName='$hdr'
+                                            AND dbActive=1"]
+    }
+    
+    
+    set dist(distributionTypes) [db eval "SELECT DistTypeName FROM DistributionTypes"]
+    
+    set carrierSetup(ShippingClass) [db eval "SELECT ShippingClass FROM ShippingClasses"]
+    set carrierSetup(ShipViaName) [db eval "SELECT ShipViaName FROM ShipVia ORDER BY ShipViaName"]
+    
+    set packagingSetup(ContainerType) [db eval "SELECT Container FROM Containers"]
+    set packagingSetup(PackageType) [db eval "SELECT Package FROM Packages"]
+    
+    # Initialize the shipOrder array; we give it the same names as what is in the Header List
+    eAssistHelper::initShipOrderArray
+    importFiles::initModVariables
+    importFiles::setJobArray
+
+} ;# importFiles::initVars
+
 proc importFiles::initModVariables {} {
     #****f* initModVariables/importFiles
     # AUTHOR
@@ -55,5 +131,32 @@ proc importFiles::initModVariables {} {
 
 } ;# initModVariables/importFiles
 
+proc importFiles::setJobArray {} {
+    #****if* setJobArray/importFiles
+    # CREATION DATE
+    #   08/12/2015 (Wednesday Aug 12)
+    #
+    # AUTHOR
+    #	Casey Ackels
+    #
+    # COPYRIGHT
+    #	(c) 2015 Casey Ackels
+    #   
+    # NOTES
+    #   Call this proc when resetting the array to default values
+    #   
+    #***
+    global log job
 
-importFiles::initModVariables
+    	array set job [list CustName "" \
+				   CustID "" \
+				   CSRName "" \
+				   Title "" \
+				   Name "" \
+				   Number "" \
+				   JobSaveFileLocation "" \
+				   TitleSaveFileLocation "" \
+				   JobFirstShipDate "" \
+				   JobBalanceShipDate ""]
+        
+} ;# importFiles::setJobArray
