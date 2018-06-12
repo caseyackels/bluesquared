53da11b82336b822f2ca40d1d0ca00ff4b2b2cf1
caseyackels <casey.ackels@gmail.com>
2018-06-12 12:04:10 -0700

    Update code for RC 3

--- EfficiencyAssist.vfs/lib/app-efficiencyassist/Modules/Update/detect_updates_code.tcl
+++ EfficiencyAssist.vfs/lib/app-efficiencyassist/Modules/Update/detect_updates_code.tcl
@@ -38,7 +38,7 @@ proc vUpdate::saveCurrentVersion {} {
     #	vUpdate::whatVersion vUpdate::getLatestRev
     #
     # PARENTS
-    #	
+    #
     #
     # NOTES
     #
@@ -47,45 +47,45 @@ proc vUpdate::saveCurrentVersion {} {
     #
     #***
     global log cVersion program job
-    
+
     if {![info exists program(dbVersion)]} {set program(dbVersion) ""}
-    
+
     set firstRun 0
     ${log}::debug Entering SaveCurrentVersion
-    
+
     # Check to see if we've ran this before ...
     if {[info exists program(Version)] == 0} {
         ${log}::debug First time starting Efficiency Assist. Skipping the version check.
 		return
     }
-	
+
 
     # We've loaded all the saved variables, so we know what the 'old' version is.
     set cVersion(oldVersion) $program(Version)
     set program(Version) [package versions app-efficiencyassist]
-	
+
     #set program(PatchLevel) 0.1 ;# Leading decimal is not needed
-    set program(beta) "RC"
-	
+    set program(beta) "RC 2"
+
     #set program(fullVersion) "$program(Version).$program(PatchLevel) $program(beta)"
 	set program(fullVersion) "$program(Version) $program(beta)"
 
     set program(Name) "Efficiency Assist"
     set program(FullName) "$program(Name) - $program(fullVersion)"
-    
+
 	# Cleanup old versions
 	if {[info exists program(PatchLevel)]} {unset program(PatchLevel)}
 
-    
+
     tk appname $program(Name)
 	${log}::debug Current program version: [package versions app-efficiencyassist]
 	${log}::debug vcompare: [package vcompare $cVersion(oldVersion) $program(Version)]
-	
+
 	if {[package vcompare $cVersion(oldVersion) $program(Version)] == -1} {
 		# We are running a new version; display an alert window
 		vUpdate::newVersion "new version" "explanation"
 	}
-	
+
 } ;#saveCurrentVersion
 
 proc vUpdate::checkDBVers {version} {
@@ -98,38 +98,38 @@ proc vUpdate::checkDBVers {version} {
     #
     # COPYRIGHT
     #	(c) 2015 Casey Ackels
-    #   
+    #
     #
     # SYNOPSIS
-    #   vUpdate::checkDB version 
+    #   vUpdate::checkDB version
     #
     # FUNCTION
     #	Ensures the DB has been updated; if it hasn't. Exit gracefully.
-    #   
-    #   
+    #
+    #
     # CHILDREN
     #	N/A
-    #   
+    #
     # PARENTS
-    #   
-    #   
+    #
+    #
     # NOTES
-    #   
-    #   
+    #
+    #
     # SEE ALSO
-    #   
-    #   
+    #
+    #
     #***
     global log program
 
     # Current schema compat version
     set getCompatVersion [eAssist_db::dbWhereQuery -columnNames ProgramVers -table Schema -where ProgramVers='$version']
-    
+
     if {$getCompatVersion eq ""} {
         ${log}::critical DB schema is not compatible with this version. Please update the database, before running $program(Name)!
         } else {
             ${log}::info DB Schema is compatible with this version of $program(Name)
         }
 
-    
+
 } ;# vUpdate::checkDBVers
