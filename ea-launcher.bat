9b451f31cd0455f5244485b01d93d8f93ed67b82
Casey <casey.ackels@gmail.com>
2020-05-04 09:09:56 -0700

    Switch out tclkits
    Switch out tclkits for 8.6.10 and update ea-launcher.bat to use the new files.

--- ea-launcher.bat
+++ ea-launcher.bat
@@ -1,7 +1,7 @@
+REM Use double %
+@echo off
+cd C:\Users\Casey\Documents\GitHub\bluesquared
+
 for /f %%i in ('dir /b/a-d/od/t:c') do set LAST=%%i
 echo The most recently created file is %LAST%
-
-echo Launching %LAST%
 Start "" %LAST%
-
-ping -n 5 127.0.0.1>nul
